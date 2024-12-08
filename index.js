626d3b06bf290879439595b3664542491ace7f24
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2024-12-08 10:13:20 +0800

    Add login feature

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const greeting = function(person){
   console.log(`Hello ${person} 😄`);
 }
 greeting('John');
+
+const login = function(password){
+  if (password === "123"){
+    console.log(`Login successfully!`);
+  } else{
+    console.log(`Invalid password, please try again`);
+  }
+}
+login('1234');
